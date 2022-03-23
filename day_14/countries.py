fc08ac883e4cf9073c2a5986b0516d7c3bb3839a
sallav <salla_mari@hotmail.com>
2022-03-23 22:07:03 +0200

    day 15

---
+++ day_14/countries.py
@@ -0,0 +1,242 @@
+import string
+from functools import reduce
+
+countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Cape Verde',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombi',
+  'Comoros',
+  'Congo (Brazzaville)',
+  'Congo',
+  'Costa Rica',
+  "Cote d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor Timur)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia, The',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Macedonia',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia and Montenegro',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Swaziland',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Taiwan',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe',
+]
+
+def categorize_countries(pattern):
+        def getctrs(lst):
+            return list(filter(lambda x: x.lower().find(pattern)>=0, lst))
+        return getctrs
+
+stans = categorize_countries('stan')
+iasc = categorize_countries('ia')
+lands = categorize_countries('land')
+islands = categorize_countries('island')
+
+print(stans(countries))
+print(iasc(countries))
+print(lands(countries))
+print(islands(countries))
+
+def starting_letters_dict(letters, lst):
+    countries_by_letter = dict()
+    for letter in letters:
+            countries_by_letter[letter.upper()] =  len(list(filter(lambda x: x.startswith(letter), lst))) 
+    return countries_by_letter
+
+ls_bl = starting_letters_dict(string.ascii_letters, countries)
+
+print(ls_bl)
+
+def get_first_ten_countries():
+    def ten(lst):
+        return reduce(lambda x, y: (x + ', ' + y), lst[:10]).split(', ')
+    return ten
+
+first_10 = get_first_ten_countries()
+
+print(first_10(countries))
+
+def get_last_ten_countries():
+    def lten(lst):
+        l = len(lst)
+        return lst[l-10:]
+    return lten        
+
+last_10 = get_last_ten_countries()
+
+print(last_10(countries))
---
+++ day_15/error_types.py
@@ -0,0 +1,52 @@
+
+# SyntaxError:
+
+# print 'hello world!'
+
+
+# NameError:
+
+# print(age)
+
+
+# IndexError:
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers[5]
+
+
+# ModuleNotFoundError:
+
+# import maths
+
+
+# AttributeError:
+
+# import math
+# math.PI 
+
+
+# KeyError:
+
+# users = {'name': 'Asab', 'age': 250, 'country': 'Finland'}
+# users['county']
+
+
+# TypeError:
+
+# 4 + '3'
+
+
+# ImportError:
+
+# from math import power
+
+
+# ValueError:
+
+# int('12a')
+
+
+# ZeroDivisionError:
+
+# 1/0
